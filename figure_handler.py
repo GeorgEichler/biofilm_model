005afa935682157b1016df890aea4fd407d656b3
GeorgEichler <georg_eichler@web.de>
2025-06-23 11:45:16 +0100

    Add figure handler file

---
+++ figure_handler.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+
+
+class FigureHandler:
+
+    def __init__(self, config):
+        self.config = config
+        plt.rcParams.update({
+            "axes.titlesize": 18,
+            "axes.labelsize": 16,
+            "xtick.labelsize": 14,
+            "ytick.labelsize": 14,
+            "legend.fontsize": 14,
+            "figure.dpi": 100 #change resolution, standard is 100
+        })
+
+    def height_profile(self, h_solutions, timestamps, savefig = False):
+        plt.figure()
+        x = self.config.mesh.coordinates().flatten
+
+        for t in timestamps:
+            plt.plot(x, h_solutions[t], label = f'Time = {t}')
+
+        plt.xlabel('x')
+        plt.ylabel('h')
+        plt.legend()
+        plt.title('Height profile')
