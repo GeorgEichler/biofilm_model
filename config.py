4dccdff1cd0b034b7c8019cb11496300c26665b2
Georg Eichler <georg_eichler@web.de>
2025-07-05 00:36:43 +0100

    Update config file

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import numpy as np
-from scipy.sparse import diags
+import copy
 
 class OneDConfig:
     """
@@ -24,51 +24,14 @@ class OneDConfig:
         self.params = {
             'L': 10, 'N': 1000, 'Q': 0.5, 'gamma': 0.1, 'h_max': 0.5, 'g': 0.1,
             'a': 0.1, 'b': np.py/2, 'c': 1.0, 'd': 0.0, 'k': 2*np.pi,
-            'h_init_type': 'gaussian' 
+            'h_init_type': 'gaussian'
         }
 
         # Update parameters with possible user-provided arguments
         self.params.update(kwargs)
 
-        self._setup_grid_and_operators()
-        self._set_initial_condition()
-
-    def _setup_grid_and_operators(self):
+    def copy(self):
         """
-        Define computational grid and finite difference matrices
+        Create a deep copy of the configuration instance
         """
-        N = self.params['N']
-        L = self.params['L']
-
-        self.dx = L / N
-        self.x = (np.arange(1, N + 1) - 0.5) * self.dx # cell centered grid
-
-        # First derivative with periodic boundary conditions
-        D = diags([-1, 0, 1], [-1, 0, 1], shape=(N, N)).toarray() / (2 * self.dx)
-        D[0, :] = 0 
-        D[0, 1] = 1 /(2 * self.dx)
-        D[0, -1] = -1 /(2 * self.dx)
-
-        D[-1, :] = 0
-        D[-1, 0] = 1 / (2 *self.dx)
-        D[-1, -2] = -1 / (2 *self.dx)
-
-        # Second derivative with periodic boundary conditions
-        main_diag = -2.0 * np.ones(N)
-        off_diag = np.ones(N - 1)
-        Lap = diags([off_diag, main_diag, off_diag], [-1, 0 , 1]).toarray() /self.dx**2
-        Lap[0, -1] = 1 / self.dx**2
-        Lap[-1, 0] = 1 / self.dx**2
-        self.Lap = Lap
-
-    def _set_initial_condition(self):
-        """Initial height profile of the thin film"""
-        L = self.params['L']
-        init_type = self.params['h_init_type']
-
-        if init_type == 'gaussian':
-            self.h_init = 0.5 + 5 * np.exp(-(self.x - L/2)**2 / 0.1)
-        elif init_type == 'constant':
-            self.h_init = np.ones_like(self.x)
-        else:
-            raise ValueError(f"Unknown h_init_type:{init_type}")
+        return copy.deepcopy(self)
