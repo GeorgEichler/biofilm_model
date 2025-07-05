e57dc22a19204de0e1fb782d212aa59e30584644
Georg Eichler <georg_eichler@web.de>
2025-07-05 16:11:03 +0100

    Replace the imperative program by a modularised one completely

--- OneD_FDM_oscillatory_model.py
+++ OneD_FDM_oscillatory_model.py
@@ -97,120 +97,14 @@ class OneD_Thin_Film_Model:
         return sol.t, sol.y
 
 
-"""
-# Parameters
-L = 10    # domain [0,L]
-N = 1000   # grid numbers
-dx = L/N  # space step size
-x = 0 + (np.arange(1, N + 1) - 0.5)*dx # cell-centred staggered grid
-T = 100          # final time
-num_steps = 100 # number of time steps
-t_eval = np.linspace(0, T, 5) 
-Q = 0.5 # diffusion coefficient
-gamma = 0.1 # surface tension
-h_max = 5 # maximal film height
-g = 0.01    # growth term
+if __name__ == "__main__":
 
-# Parameters for binding potential
-a = 0.1
-b = np.pi/2
-c = 1
-d = 0
-k = 2*np.pi
+    config = OneDConfig()
+    model = OneD_Thin_Film_Model(config)
 
-# Initial conditions
-h_init = np.ones_like(x)
-h_init = 0.5 + 5 * np.exp(-(x - L/2)**2/0.1)
-#h_init = 0.1 - 0.001 * (x - L/2)**2
+    times, H = model.solve(model.h_init)
 
-# First derivative with periodic boundary conditions
-D = diags([-1, 0, 1], [-1, 0, 1], shape=(N, N)).toarray() / (2 * dx)
-D[0, :] = 0 
-D[0, 1] = 1 /(2 * dx)
-D[0, -1] = -1 /(2 * dx)
+    figure_handler = fh.FigureHandler(model)
+    figure_handler.plot_profiles(H, times)
 
-D[-1, :] = 0
-D[-1, 0] = 1 / (2 * dx)
-D[-1, -2] = -1 / (2 * dx)
-
-# Second derivative with periodic boundary conditions
-main_diag = -2.0 * np.ones(N)
-off_diag = np.ones(N - 1)
-Laplacian = diags([off_diag, main_diag, off_diag], [-1, 0 , 1]).toarray() / dx**2
-Laplacian[0, -1] = 1 / (dx**2)
-Laplacian[-1, 0] = 1 / (dx**2)
-
-
-def g1(a, b, c, d, k, h):
-    return a * np.cos(h * k + b) * np.exp(-h/c) + d * np.exp(-h/(2*c))
-
-def g2(a, b, c, d, k, h):
-    return a * np.cos(h * k + b) * np.exp(-h/c) + d * np.exp(-2*h/c)
-
-
-def Pi1(a, b, c, d, k, h):
-    return a * np.exp(-h/c) * (k * np.sin(h * k + b) + 1/c * np.cos(h * k + b)) + d/(2*c)*np.exp(-h/(2*c))
-
-def Pi2(a, b, c, d, k, h):
-    return a * np.exp(-h/c) * (k * np.sin(h * k + b) + 1/c * np.cos(h * k + b)) + (2*d)/(c)*np.exp(-(2*h)/(c))
-
-# RHS of thin film equation, assumes h to be a numpy array
-def rhs_thin_film_eq(t, h):
-    h_xx = Laplacian @ h
-    f = -Pi1(a, b, c, d, k, h) - gamma * h_xx
-    f_x =  D @ f
-    flux = D @ (Q * f_x)
-    source = g * h * (1 - h/h_max)
-    return flux + source
-
-def free_energy(h):
-    dhdx = D @ h
-    integrand = 0.5 * gamma * dhdx**2 + g1(a, b, c, d, k, h)
-
-    return np.sum(integrand) * dx
-
-# Solution of the remaining ODE
-sol = solve_ivp(rhs_thin_film_eq, [0, T], h_init,  method = 'BDF', t_eval=t_eval)
-times = sol.t
-H  = sol.y
-F_values = np.array([free_energy(H[:,i]) for i in range(len(times))])
-
-plt.figure()
-plt.plot(times, F_values, '-o')
-plt.xlabel('t')
-plt.ylabel('F[h(t)]')
-plt.title('Free energy evolution')
-plt.grid(True)
-
-# plot binding potential
-h_array = np.linspace(0, 10, 1001)
-plt.figure()
-plt.plot(h_array, g1(a, b, c, d, k, h_array))
-plt.xlabel('h')
-plt.ylabel('g(h)')
-plt.title("Binding potential")
-
-# Plot solution
-n_plots = 5
-idxs    = np.linspace(0, len(times)-1, n_plots, dtype=int)
-
-plt.figure()
-for i in idxs:
-    plt.plot(x, H[:, i], label=f"t = {times[i]:.2f}")
-plt.xlabel("x")
-plt.ylabel("h(x,t)")
-plt.title("Thin-film height profile")
-plt.legend()
-plt.grid()
-plt.show()
-"""
-
-
-config = OneDConfig()
-model = OneD_Thin_Film_Model(config)
-
-times, H = model.solve(model.h_init)
-
-figure_handler = fh.FigureHandler(model)
-figure_handler.plot_profiles(H, times)
-plt.show()
+    plt.show()
