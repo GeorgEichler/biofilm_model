567b6cd53e3555126d12444dae578164b870d1ac
Georg Eichler <georg_eichler@web.de>
2025-07-25 12:00:55 +0100

    Adapt spectral method to the new model base class

--- OneD_FFT_simple_model.py
+++ OneD_FFT_simple_model.py
@@ -1,108 +1,38 @@
 import numpy as np
 from scipy.fft import fft, ifft, fftfreq
 import matplotlib.pyplot as plt
+from OneD_thin_film_model import OneD_Base_Model
 from helper_functions import find_first_k_minima
 import figure_handler as fh
 from tqdm import tqdm
 import time
 
-class FFT_OneD_Thin_Film_Model:
+class FFT_OneD_Thin_Film_Model(OneD_Base_Model):
     """
     Class to solve the 1D thin-film equation using fast Fourier transform
     with a implicit-explicit Euler scheme
-    """
-
-    def __init__(self, **kwargs):
-        """
-        Kwargs:
-            L (float): Domain length [0, L]
-            N (int): Number of grid points
-            gamma (float): Surface tension
-            h_max (float): maximal film height for the growth term
-            g (float): coefficient of logistic growth, qoutient of growth and diffusion coefficient
-            a, b, c, d, k (float): Parameter for the binding potential
-            h_init_type (str): Type of inital condition
-        """
-
-        # Default parameters
-        self.params = {
-            'L': 100, 'N': 1024, 'gamma': 0.5, 'h_max': 5, 'g': 0.1, 'Q':1,
-            'a': 0.1, 'b': np.pi/2, 'c': 1, 'd': 0.02, 'k': 2*np.pi,
-            'amplitude': 2, 'var': 10, 'dt': 0.1
-        }
-        self.params.update(kwargs)
-        self._setup_grid_and_fft()
+    """ 
 
 
-        # Calulate the first minima of the binding potential
-        min, _ = find_first_k_minima(2, self.g1)
-        self.h0 = min[0] # corresponds to 'zeroth' layer
-        self.h1 = min[1] # corresponds to first layer
-
-    def _setup_grid_and_fft(self):
+    def _setup_numerical_operators(self):
+        """Calculate the wavenumbers for the FFT"""
         p = self.params
-        self.dx = p['L'] / p['N']
-        self.x = (np.arange(1, p['N'] + 1) - 0.5) * self.dx
 
         # wavenumbers for FFT
         k = 2*np.pi * fftfreq(p['N'], d = self.dx)
         self.k2 = k**2
         self.k4 = k**4
 
-    # Pre-defined initial conditions
-    def setup_initial_conditions(self, init_type):
-        L = self.params['L']
-        amplitude = self.params['amplitude']
-        var = self.params['var']
-
-        if init_type == 'gaussian':
-            h_init = (self.h0 + 0.01) + amplitude * np.exp(-(self.x - L/2)**2/var)
-        elif init_type == 'constant':
-            h_init = np.ones_like(self.x)
-        elif init_type == 'cap':
-            h_init = np.maximum(self.h0 + 0.1, amplitude - 1/amplitude * (self.x - L/2)**2)
-        else:
-            raise ValueError(f"Unknown initial condition type: {init_type}")
-        
-        return h_init
     
-    # Define binding energies and corresponding disjoint pressures
-    def f1(self, h):
-        p = self.params
-        a = p['a']; b = p['b']; c = p['c']; d = p['d']; k = p['k']
-        return a * np.cos(h * k + b) * np.exp(-h/c) + d * np.exp(-h/(2*c))
-
-    def f2(self, h):
-        p = self.params
-        a = p['a']; b = p['b']; c = p['c']; d = p['d']; k = p['k']
-        return a * np.cos(h * k + b) * np.exp(-h/c) + d * np.exp(-2*h/c)
-
-    def Pi1(self, h):
-        p = self.params
-        a = p['a']; b = p['b']; c = p['c']; d = p['d']; k = p['k']
-        return a * np.exp(-h/c) * (k * np.sin(h * k + b) + 1/c * np.cos(h * k + b)) + d/(2*c)*np.exp(-h/(2*c))
-
-    def Pi2(self, h):
-        p = self.params
-        a = p['a']; b = p['b']; c = p['c']; d = p['d']; k = p['k']
-        return a * np.exp(-h/c) * (k * np.sin(h * k + b) + 1/c * np.cos(h * k + b)) + (2*d)/(c)*np.exp(-(2*h)/(c))
-    
-    def growth_term(self, h):
-        p = self.params
-        growth = (p['g'] * h *
-                  (1 - h / p['h_max']) *
-                  (1 - self.h1 / h) *
-                  (1 - np.exp(self.h0 - h))
-        )
-        return growth
-    
-    def time_step(self, h):
+    def _time_step(self, h_hat):
         p = self.params
         dt = p['dt']
-        Q = p['Q']
+
+        # Inverse transform to calculate non-linear terms in real space
+        h = ifft(h_hat).real
 
         # calculate explicit part in real space
-        pi_h = self.Pi1(h)
+        pi_h = self.Pi(h)
         growth_h = self.growth_term(h)
 
         # Fourier transform
@@ -110,15 +40,14 @@ class FFT_OneD_Thin_Film_Model:
         growth_hat = fft(growth_h)
 
         # get explicit part -laplacian(Pi) + growth -> k2*pi_hat + growth_hat
-        N_hat = Q * self.k2 * pi_hat + growth_hat
+        N_hat = self.k2 * pi_hat + growth_hat
 
         # solve biharmonic term implicitely
-        h_hat = fft(h)
-        denom = 1 + dt * Q* p['gamma'] * self.k4
+        denom = 1 + dt * p['gamma'] * self.k4
         h_hat_new = (h_hat + dt * N_hat) / denom
 
         # transform back to real space
-        return ifft(h_hat_new).real
+        return h_hat_new
 
     def solve(self, h0, T, t_eval):
         dt = self.params['dt']
@@ -154,17 +83,20 @@ class FFT_OneD_Thin_Film_Model:
             t_snapshots.append(0.0)
             h_snapshots.append(h.copy())
 
+        # Use Fourier transform for for the solver
+        h_hat = fft(h)
+
         start = time.time()
         print(f"Start integration using spectral methods in [0, {T}] with dt = {dt}...")
         for i in tqdm(range(num_steps), desc = "FFT Simulation"):
             # perform one time step
-            h = self.time_step(h)
+            h_hat = self._time_step(h_hat)
 
             # check if we want a snapshot
             if target_ptr < len(target_indices) and i == target_indices[target_ptr]:
                 current_t = (i + 1) * dt
                 t_snapshots.append(current_t)
-                h_snapshots.append(h.copy())
+                h_snapshots.append(ifft(h_hat).real.copy())
                 target_ptr +=1
         end = time.time()
         print(f"Integration finished in {end - start:.3f}s.")
@@ -181,7 +113,7 @@ if __name__ == "__main__":
     T = 100
     # be careful with size of timestep for the implicit part
     t_eval = np.linspace(0, T, 5)
-    params = {'amplitude': 1.5, 'g': 10**(-3), 'gamma': 1, 'dt': 0.01}
+    params = {'amplitude': 1.5, 'g': 0.1, 'gamma': 1, 'dt': 0.0001}
 
     model = FFT_OneD_Thin_Film_Model(**params)
     h0 = model.setup_initial_conditions('gaussian')
@@ -193,7 +125,7 @@ if __name__ == "__main__":
     
     h_mins, g_mins = find_first_k_minima(
         k_minima = 5,
-        f = model.g1
+        f = model.f
     )
 
     figure_handler = fh.FigureHandler(model)
