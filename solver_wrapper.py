33770a84991de4f95290d09773d1aa511559ed45
Georg Eichler <georg_eichler@web.de>
2025-07-26 18:13:26 +0100

    Fix bug of the wrapper function to not return a RHS function f(t,y)

--- solver_wrapper.py
+++ solver_wrapper.py
@@ -16,8 +16,8 @@ class SolveIVPProgressWrapper:
             percent_done = (t / self.t_end) * 100
             if percent_done >= self.last_reported_percent + self.report_step:
                 self.last_reported_percent = int(percent_done / self.report_step) * self.report_step
-                elapsed_time = time.time()
+                elapsed_time = time.time() - self.start_time
                 # estimate of expected time to end integration
                 eta_seconds = (elapsed_time / percent_done * (100 - percent_done)) if percent_done > 0 else float('inf')
                 print(f" Solver Progress: {self.last_reported_percent:.0f}% (t={t:.2f}, ETA: {eta_seconds:.1f}s)  ", end = '\r')
-        return self.rhs_func
+        return self.rhs_func(t, y)
